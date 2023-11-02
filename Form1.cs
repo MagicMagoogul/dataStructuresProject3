44737d0274ba5af3cfd5e4b483b8130af1a1bb69
MagicMagoogul <owenrm1@etsu.edu>
2023-11-02 13:47:00 -0400

    GUI
    This is the GUI :)

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIDEMO
+{
+    public partial class Form1 : Form
+    {
+        public string testResults = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Runs the simulation once button is clicked
+            MessageBox.Show("Simulation Running...");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //As the simulation runs this moves forward
+
+            //delegates 
+            //asycronis -- wait task 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"{testResults}";
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIDEMO
+{
+    public partial class Form2 : Form
+    {
+        public string label1_Text = "Button Clicked";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Results...";
+        }
+    }
+}
